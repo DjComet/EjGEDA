240dd085325606b4c2439bc353950a3121c9070b
Jano <Jano@192.168.1.34>
2018-01-10 22:24:39 +0100

    pasajero

--- EjG-aabad/src/Pasajero.java
+++ EjG-aabad/src/Pasajero.java
@@ -4,4 +4,10 @@ public class Pasajero {
 	public int id;
 	public String nombre;
 	public boolean premium;
+	
+	public void pasajero(int id, String nombre, boolean premium) {
+		this.id = id;
+		this.nombre = nombre;
+		this.premium = premium;
+	}
 }
