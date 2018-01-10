6d8a5fa7138325574b898b883852684481b120bb
Jano <Jano@192.168.1.34>
2018-01-10 23:02:54 +0100

    contstructor pasajero

--- EjG-aabad/src/Pasajero.java
+++ EjG-aabad/src/Pasajero.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Pasajero {
 
@@ -5,9 +8,37 @@ public class Pasajero {
 	public String nombre;
 	public boolean premium;
 	
-	public void pasajero(int id, String nombre, boolean premium) {
+	public Pasajero(int id, String nombre, boolean premium) {
 		this.id = id;
 		this.nombre = nombre;
 		this.premium = premium;
+	}
+	
+	public Pasajero() throws IOException {
+		premium = false;
+		
+		System.out.println("Escribe el nombre del pasajero: ");
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader (isr);
+		nombre = br.readLine();
+		
+		System.out.println("Escribe el id del pasajero: ");
+		isr = new InputStreamReader(System.in);
+		br = new BufferedReader (isr);
+		id = Integer.parseInt(br.readLine());
+		
+		String cadena = null;
+		
+		while(!cadena.equals("Y") || !cadena.equals("y") ||!cadena.equals("N") ||!cadena.equals("n")) {
+		System.out.println("¿Es premium? (Y/N): ");
+		isr = new InputStreamReader(System.in);
+		br = new BufferedReader (isr);
+		cadena = br.readLine();
+		}
+		
+		if(cadena.equals("Y") || cadena.equals("y")) {
+			premium = true;
+		}
+		
 	}
 }
--- EjG-aabad/src/Principal.java
+++ EjG-aabad/src/Principal.java
@@ -1,9 +1,15 @@
+import java.io.IOException;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
+		Pasajero p = new Pasajero();
+		Pasajero p1 = new Pasajero();
+		Pasajero p3 = new Pasajero();
+		Pasajero p2 = new Pasajero(1, "gonzalo", true);
+		
 	}
 
 }
