9334f1863771318104ed8363b64c9fa940e61b15
Jano <Jano@192.168.1.34>
2018-01-11 01:04:21 +0100

    arreglos

---
+++ EjG-aabad/src/Ordenacion.java
@@ -0,0 +1,33 @@
+
+public class Ordenacion {
+	
+	void quicksort(String a[],int primero,int ultimo)
+	{
+	 int i,j, central;
+	 String tmp,pivote;
+
+	 central = (primero+ultimo)/2;
+	 pivote=a[central];
+	 
+	 i=primero;
+	 j=ultimo;
+	 
+	 do{
+		 while((a[i].compareTo(pivote)<0) && (i<ultimo)) i++;
+		 while((a[j].compareTo(pivote)>0) && (j<ultimo)) i++;
+		 
+		 if(i<=j)
+		 {
+			 tmp = a[i];
+			 a[i] = a[j];
+			 a[j] = tmp;
+			 i++;
+			 j--;
+		 }
+	 }while(i<=j);
+
+	 if(primero<j) quicksort(a,primero,j);
+	 if(i<ultimo) quicksort(a,i,ultimo);
+	}
+
+}
